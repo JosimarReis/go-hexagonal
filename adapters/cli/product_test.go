c8a47efb4dbebee1b18012598851a1844de01941
Josimar Reis <josimargeraldoreis@gmail.com>
2025-11-18 15:06:03 -0300

    Add CLI adapter for product management with tests and dependency updates

---
+++ adapters/cli/product_test.go
@@ -0,0 +1,56 @@
+package cli_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/JosimarReis/go-hexagonal/adapters/cli"
+	mock_application "github.com/JosimarReis/go-hexagonal/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRun(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	productName := "Product Test"
+	productPrice := 25.99
+	productStatus := "enabled"
+	productId := "abc"
+
+	productMock := mock_application.NewMockProductInferface(ctrl)
+	productMock.EXPECT().GetID().Return(productId).AnyTimes()
+	productMock.EXPECT().GetName().Return(productName).AnyTimes()
+	productMock.EXPECT().GetStatus().Return(productStatus).AnyTimes()
+	productMock.EXPECT().GetPrice().Return(productPrice).AnyTimes()
+
+	service := mock_application.NewMockProductServiceInterface(ctrl)
+	service.EXPECT().Create(productName, productPrice).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Get(productId).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Enable(productMock).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Disable(productMock).Return(productMock, nil).AnyTimes()
+
+	//test cli create
+	resultExpected := fmt.Sprintf("Product ID %s with the name %s has been created with the price %f and status %s",
+		productId, productName, productPrice, productStatus)
+
+	result, err := cli.Run(service, "create", "", productName, productPrice)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product %s has been enabled", productName)
+	result, err = cli.Run(service, "enable", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product %s has been disabled", productName)
+	result, err = cli.Run(service, "disable", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product ID: %s, Name: %s, Price: %f, Status: %s", productId, productName, productPrice, productStatus)
+	result, err = cli.Run(service, "get", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/JosimarReis/go-hexagonal
 go 1.16
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/mock v1.6.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.32 // indirect
-	github.com/satori/go.uuid v1.2.0 // indirect
-	github.com/stretchr/testify v1.7.0
+	github.com/golang/mock v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.11.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
